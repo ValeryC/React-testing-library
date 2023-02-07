5df37d8638a09a533191e8dd0fcf908b0c4c2e69
chin <valery.chin@external.totalenergies.com>
2023-02-07 16:44:11 +0100

    [test]add test for checkbox disable

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
+  const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div>
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
-      >
+        disabled={disabled}>
         Change to {newButtonColor}
       </button>
-      <input type="checkbox" />
+      <input 
+      type="checkbox" 
+      id="disable-button-checkbox"
+      defaultChecked={disabled}
+      onChange={(e) => setDisabled(e.target.checked)} />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
